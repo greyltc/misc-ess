1fac5f662d14dda6c79fb886cfeb91ff4c33f84d
Grey Christoforo <grey@christoforo.net>
2017-11-13 15:35:53 +0100

    upgrade current measurement code

--- getCurrent.py
+++ getCurrent.py
@@ -5,92 +5,34 @@
 
 # prints current from keithley 24xx connected via serial
 
-import serial
-import argparse
-import signal
+from k24xx import K24xx
 import sys
-import time
-import inspect
-
-port = '/dev/ttyUSB0'
-baud = 9600
-timeout = 3 # seconds
-
-expectedDeviceString = b'KEITHLEY INSTRUMENTS INC.,MODEL 2410,4090615,C33   Mar 31 2015 09:32:39/A02  /J/K\r\n'
+import argparse
 
 # for cmd line arguments
 parser = argparse.ArgumentParser(description='Reads current from keithley 24xx sourcemeter.')
-parser.add_argument('-c','--continuous', dest='continuous', action='store_true', default=False, help="Operate continuously (forever)")
+parser.add_argument('-p',dest='port', type=str, default='/dev/ttyUSB0', help='Serial port the sourcemeter is connected to')
+parser.add_argument('-b',dest='baud', type=int, default=9600, help='Serial port comms baud rate')
+parser.add_argument('-t',dest='timeout', type=int, default=30, help='Comms timeout [s]')
+parser.add_argument('-a',dest='meanValues', type=int, default=1, help='Number of values to internally average')
+parser.add_argument('-nplc',dest='nplc', type=float, default=10.0, help='Number of power line cycles to integrate over when measuring')
+parser.add_argument('-n',dest='n', type=int, default=1, help='Number of measurements to make (-1 for infinty)')
 args = parser.parse_args()
 
-k = serial.Serial(port,baud,timeout=timeout)
+k = K24xx(baud=args.baud, port=args.port, timeout=args.timeout)
 
-k.write(b'*RST\n')
-k.write(b'*IDN?\n')
-deviceString = k.readline()
+k.currentSetup(nplc=args.nplc, nMean=args.meanValues)
+k.setOutout(True)
 
-if deviceString == expectedDeviceString:
-  pass
-  #print('Conected to', deviceString.decode("utf-8"))
-else:
-  print('ERROR: Got unexpected device string:', deviceString.decode("utf-8"))
-  sys.exit(-1)
-
-cmds = []
-
-cmds.append(':SOUR:FUNC VOLT')
-cmds.append(':SOUR:VOLT:MODE FIXED')
-cmds.append(':SENS:FUNC "CURR"')
-cmds.append(':SENS:CURR:NPLC 10')
-cmds.append(':SOUR:VOLT:RANG MIN')
-cmds.append(':SOUR:VOLT:LEV 0')
-cmds.append(':FORM:ELEM CURR')
-cmds.append(':OUTP ON')
-
-def cleanup(k):
-  cmd = ':OUTP OFF'
-  k.write(cmd.encode('utf-8') + b'\n')
-  #k.write(b':SYST:ERR?\n')
-  #err = k.readline()
-  #print(err)
-  k.close()
-
-def signal_handler(signal, frame):
-  #print('You pressed Ctrl+C!')
-  #print (frame.f_locals)
-  k = frame.f_locals['self']
-  junk = k.readline()
-  cleanup(k)
-
-  sys.exit(0) 
-
-# register handler for ctrl+c cleanup
-signal.signal(signal.SIGINT, signal_handler) 
-
-for cmd in cmds:
-  #print('Sending', cmd)
-  cmd = cmd.encode('utf-8') + b'\n'
-  k.write(cmd)
-  #k.write(b':SYST:ERR?\n')
-  #err = k.readline()
-  #print(err)
-
-if args.continuous:
-  print('Running continuously forever. Use Ctrl+c to terminate')
-
-# delay aftetr output on
-time.sleep(0.5)
-
+m = 0
 while True:
-  cmd = ':READ?'
-  k.write(cmd.encode('utf-8') + b'\n')
-  current = float(k.readline().decode('utf-8'))
-
+  current = k.getCurrent()
   print(current)
+  m = m+1
 
-  if not args.continuous:
+  if args.n == m:
       break
 
-cleanup(k)
+k.setOutout(False)
 
 sys.exit(0)
---
+++ k24xx.py
@@ -0,0 +1,97 @@
+# written by grey@christoforo.net
+# on 8 Nov 2017
+
+import serial
+import time
+
+class K24xx:
+  """keithley 24xx library
+  """
+  port = None
+  expectedDeviceString = 'KEITHLEY INSTRUMENTS INC.,MODEL 2410,4090615,C33   Mar 31 2015 09:32:39/A02  /J/K\r\n'
+  outOnSettleTime = 0.5 # seconds to settle after output turned on
+  def __init__(self, port='/dev/ttyUSB0', baud=9600, timeout=30):
+    """keithley 24xx library constructor
+    """    
+    self.port = serial.Serial(port,baud,timeout=timeout)
+    identStr = self._qu('*IDN?')
+    
+    if identStr == self.expectedDeviceString:
+      pass
+      #print('Conected to', deviceString.decode("utf-8"))
+    else:
+      print('ERROR: Got unexpected device string:', identStr)
+      self.port.close()
+      
+  def __del__(self):
+    """keithley 24xx library deconstructor
+    """
+    #print('Cleaning up sourcemeter comms')
+    try:
+      self._write(':OUTP OFF')
+      self.port.flush()
+      time.sleep(0.1)
+    except:
+      pass
+    try:
+      self.port.close()
+    except:
+      pass
+    
+  def _write(self,cmd):
+    """Send command to sourcemeter
+    """
+    toSend = cmd.encode('utf-8') + b'\n'
+    self.port.write(toSend)
+    
+  def _qu(self,cmd):
+    """Query sourcemeter with command
+    """    
+    self._write(cmd)
+    result = self.port.readline()
+    decoded = result.decode('utf-8')
+    return(decoded)
+    
+  def currentSetup(self,nplc=10.0,nMean=1):
+    """Setup sourcemeter for current measurements
+    """
+    cmds = []
+    cmds.append('*RST')
+    cmds.append(':SOUR:FUNC VOLT')
+    cmds.append(':SOUR:VOLT:MODE FIXED')
+    cmds.append(':SENS:FUNC "CURR"')
+    cmds.append(':SENS:CURR:NPLC {:}'.format(nplc))
+    cmds.append(':SOUR:VOLT:RANG MIN')
+    cmds.append(':SOUR:VOLT:LEV 0')
+    cmds.append(':FORM:ELEM CURR')
+    
+    nMean = int(nMean)
+    if nMean == 1:
+      cmds.append(':SENS:AVER:STAT 0')
+    elif (nMean > 1) and (nMean <= 100):
+      cmds.append(':SENS:AVER:STAT 1')
+      cmds.append(':SENS:AVER:STAT {:}'.format(nMean))
+    else:
+      print('ERROR: Got invalid nMean value')
+    
+    for cmd in cmds:
+      #print('Sending', cmd)
+      self._write(cmd)
+      #self.port.write(b':SYST:ERR?\n')
+      #err = self.port.readline()
+      #print(err)
+
+  def setOutout(self,value):
+    if value == True:
+      self._write(':OUTP ON')
+      time.sleep(self.outOnSettleTime) # let the output settle
+    elif value == False:
+      self._write(':OUTP OFF')
+    else:
+      print('Invalid data type')
+      
+  def getCurrent(self):
+    """Reads current from sourcemeter
+    """    
+    current = self._qu('READ?')
+    return float(current)
